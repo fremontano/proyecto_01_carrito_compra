cfbfdb9c2f76a00f8bc7d069ee19c77a3c8b0eb8
freyder_montano <fremontano24@gmail.con>
2024-09-07 10:55:37 -0400

    funcion paracalcular el precio total del carrito de compra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
-
 export const Header = ({ cart }) => {
+    // Estado derivado para saber si el carrito esta vacio
+    const isEmpty = () => cart.length === 0;
 
+    // Funcion para sumar los productos
+    const cartTotalPrecio = () => cart.reduce((total, item) => total + (item.quantity * item.price), 0);
 
 
     return (
@@ -9,76 +12,62 @@ export const Header = ({ cart }) => {
                 <div className="row justify-content-center justify-content-md-between">
                     <div className="col-8 col-md-3">
                         <a href="index.html">
-                            <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo" />
+                            <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
                         </a>
                     </div>
                     <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                        <div
-                            className="carrito"
-                        >
-                            <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+                        <div className="carrito">
+                            <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
                             <div id="carrito" className="bg-white p-3">
-                                <p className="text-center">El carrito esta vacio</p>
-                                {/* iterando el carrito  */}
-                                <table className="w-100 table">
+                                {isEmpty() ? (
+                                    <p className="text-center">El carrito está vacío</p>
+                                ) : (
+                                    <>
+                                        {/* Iterando el carrito */}
+                                        <table className="w-100 table">
+                                            <thead>
+                                                <tr>
+                                                    <th>Imagen</th>
+                                                    <th>Nombre</th>
+                                                    <th>Precio</th>
+                                                    <th>Cantidad</th>
+                                                    <th></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {cart.map((guitar) => (
+                                                    <tr key={guitar.id}>
+                                                        <td>
+                                                            <img className="img-fluid" src={`/img/${guitar.image}.jpg`} alt="imagen guitarra" />
+                                                        </td>
+                                                        <td>{guitar.name}</td>
+                                                        <td className="fw-bold">${guitar.price}</td>
+                                                        <td className="d-flex align-items-start gap-4">
+                                                            <button type="button" className="btn btn-dark">-</button>
+                                                            {guitar.quantity}
+                                                            <button type="button" className="btn btn-dark">+</button>
+                                                        </td>
+                                                        <td>
+                                                            <button className="btn btn-danger" type="button">X</button>
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
 
-                                    <thead>
-                                        <tr>
-                                            <th>Imagen</th>
-                                            <th>Nombre</th>
-                                            <th>Precio</th>
-                                            <th>Cantidad</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {
-                                            cart.map((guitar) => (
-                                                <tr key={guitar.id}>
-                                                    <td>
-                                                        <img className="img-fluid" src={`/img/${guitar.image}.jpg`} alt="imagen guitarra" />
-                                                    </td>
-                                                    <td>{guitar.name}</td>
-                                                    <td className="fw-bold">
-                                                        ${guitar.price}
-                                                    </td>
-                                                    <td className="flex align-items-start gap-4">
-                                                        <button
-                                                            type="button"
-                                                            className="btn btn-dark"
-                                                        >
-                                                            -
-                                                        </button>
-                                                        {guitar.quantity}
-                                                        <button
-                                                            type="button"
-                                                            className="btn btn-dark"
-                                                        >
-                                                            +
-                                                        </button>
-                                                    </td>
-                                                    <td>
-                                                        <button
-                                                            className="btn btn-danger"
-                                                            type="button"
-                                                        >
-                                                            X
-                                                        </button>
-                                                    </td>
-                                                </tr>))
-                                        }
-                                    </tbody>
-                                </table>
-
-                                <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
-                                <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                                        {/* Precio total del carrito */}
+                                        <p className="text-end">
+                                            Total a pagar: <span className="fw-bold">${cartTotalPrecio()}</span>
+                                        </p>
+                                        <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                                    </>
+                                )}
                             </div>
                         </div>
                     </nav>
                 </div>
             </div>
         </header>
-
-    )
-}
+    );
+};
