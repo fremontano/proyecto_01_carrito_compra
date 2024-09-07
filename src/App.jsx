1f44b0d5163558b5ba71526e9966cd9bdb151f07
freyder_montano <fremontano24@gmail.con>
2024-09-07 13:10:34 -0400

    Ajuste en el carrito de compras: se creó la funcionalidad para aumentar y disminuir la cantidad de artículos con botones dedicados

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,18 @@ function App() {
   // Aquí se almacenarán los datos de las guitarras en un array
   const [data, setData] = useState(dbGuitars);
   const [cart, setCart] = useState([]);
+
+  const MAX_ITEMS = 10;
+  const MIN_ITEMS = 1;
+
   console.log(cart)
 
 
 
 
-
+  // agregar item al carrito 
   const addToCart = (items) => {
-
     const itemExiste = cart.findIndex(guitar => guitar.id === items.id);
-
-
     if (itemExiste >= 0) {
       const updateCart = [...cart];
       updateCart[itemExiste].quantity += 1;
@@ -28,7 +29,39 @@ function App() {
       items.quantity = 1;
       setCart([...cart, items]);
     }
+  }
 
+
+  // eliminar item del carrito 
+  const removeFromCart = (id) => {
+    setCart(cart.filter(guitar => guitar.id !== id));
+  }
+
+  //incrementar item del carrito
+  const incrementarQuantity = (id) => {
+    const updateCart = cart.map((item) => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      //para no perder la referencia de los demas item del carrito
+      return item;
+    });
+
+    //setearla al carrito
+    setCart(updateCart);
+  }
+
+  // decrementar item del carrito
+  const decrementarQuantity = (id) => {
+    const updateCart = cart.map((item) => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return { ...item, quantity: item.quantity - 1 };
+      }
+      return item;
+    });
+
+    //setearla al carrito
+    setCart(updateCart);
   }
 
 
@@ -38,6 +71,9 @@ function App() {
 
       <Header
         cart={cart}
+        removeFromCart={removeFromCart}
+        incrementarQuantity={incrementarQuantity}
+        decrementarQuantity={decrementarQuantity}
       />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { useMemo } from "react";
 
-export const Header = ({ cart }) => {
+export const Header = ({ cart, removeFromCart, incrementarQuantity, decrementarQuantity }) => {
     // Estado derivado que indica si el carrito está vacío
     const isEmpty = useMemo(() => cart.length === 0, [cart]);
 
     // Función que calcula el precio total de los productos en el carrito
     const cartTotalPrecio = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart]);
+
 
 
     return (
@@ -46,12 +47,27 @@ export const Header = ({ cart }) => {
                                                         <td>{guitar.name}</td>
                                                         <td className="fw-bold">${guitar.price}</td>
                                                         <td className="d-flex align-items-start gap-4">
-                                                            <button type="button" className="btn btn-dark">-</button>
+                                                            <button
+                                                                type="button"
+                                                                className="btn btn-dark"
+                                                                onClick={() => decrementarQuantity(guitar.id)}
+                                                            >-</button>
                                                             {guitar.quantity}
-                                                            <button type="button" className="btn btn-dark">+</button>
+                                                            <button
+                                                                type="button"
+                                                                className="btn btn-dark"
+                                                                onClick={() => incrementarQuantity(guitar.id)}
+                                                            >
+                                                                +</button>
+
                                                         </td>
                                                         <td>
-                                                            <button className="btn btn-danger" type="button">X</button>
+                                                            <button
+                                                                className="btn btn-danger"
+                                                                type="button"
+                                                                onClick={() => removeFromCart(guitar.id)}
+                                                            >
+                                                                X</button>
                                                         </td>
                                                     </tr>
                                                 ))}
