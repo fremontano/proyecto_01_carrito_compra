865d0fe2a5b66e28ab5e37d9b87a35bc1897d5dc
freyder_montano <fremontano24@gmail.con>
2024-09-07 02:15:38 -0400

    creando metodo agregar al carrito de compra

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react"
 import { Footer } from "./components/Footer"
 import { Guitar } from "./components/Guitar"
 import { Header } from "./components/Header"
-
+import { dbGuitars } from "./data/db";
 
 function App() {
+
+  // Aquí se almacenarán los datos de las guitarras en un array
+  const [data, setData] = useState(dbGuitars);
+  const [cart, setCart] = useState([]);
+  console.log(cart)
+
+
+
+
+
+  const addToCart = (items) => {
+
+    const itemExiste = cart.findIndex(guitar => guitar.id === items.id);
+
+
+    if (itemExiste >= 0) {
+      const updateCart = [...cart];
+      updateCart[itemExiste].quantity += 1;
+      setCart(updateCart);
+    } else {
+      items.quantity = 1;
+      setCart([...cart, items]);
+    }
+
+  }
+
+
 
   return (
     <>
@@ -11,7 +39,19 @@ function App() {
       <Header />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
-        <Guitar />
+        <div className="row mt-5">
+          {/* Cada guitarra se renderiza aquí */}
+          {
+            data.map((guitar) => (
+              <Guitar
+                key={guitar.id}
+                guitar={guitar}
+                setCart={setCart}
+                addToCart={addToCart}
+              />
+            ))
+          }
+        </div>
 
       </main >
       <Footer />
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,17 +1,25 @@
 
-export const Guitar = () => {
+export const Guitar = ({ guitar, addToCart, setCart }) => {
+
+
+    const { id, name, image, description, price } = guitar;
+
+
+
+
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-                <img className="img-fluid" src="./public/img/guitarra_02.jpg" alt="imagen guitarra" />
+                <img className="img-fluid" src={`/public/img/${image}.jpg`} alt="imagen guitarra" />
             </div>
             <div className="col-8">
-                <h3 className="text-black fs-4 fw-bold text-uppercase">SRV</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                <p className="fw-black text-primary fs-3">$299</p>
+                <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                <p>{description}</p>
+                <p className="fw-black text-primary fs-3">${price}</p>
                 <button
                     type="button"
                     className="btn btn-dark w-100 "
+                    onClick={() => { addToCart(guitar) }}
                 >Agregar al Carrito</button>
             </div>
         </div>
